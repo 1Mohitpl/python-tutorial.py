2a4f7c1ba3e86ef9fdc25ff2a3e875cf99d9022c
Mohit kumar paul <mohit1paul@gmail.com>
2024-03-21 23:34:20 +0530

    practice some topics

---
+++ ascii.py
@@ -0,0 +1,11 @@
+char = "a"
+print(ord(char))
+char = "A"
+print(ord(char))  # ord is a function which is helps to find out the ascii value of any char
+
+
+ascii = 98
+print(chr(ascii))
+ascii = 65
+print(chr(ascii))
+
--- strings.py
+++ strings.py
@@ -1,7 +1,12 @@
 #straing methodes
-course= 'pthon for beginners'
-course
-print(course.upper())
+# course= 'pthon for beginners'
+# course
+# print(course.upper())
+
+name = input("Enter an your name :  ")
+age = input ("Enter your age: ")
+print(type(age))
+print( "My name is "  + name + " " + " and my age is " + age)
 
 
 
@@ -81,7 +86,7 @@ print(course.upper())
 # print(name[1:-1])
 
 
-#dynamically insert value into string 
+# #dynamically insert value into string 
 # first = ' mohiyt '
 # last = 'paul '
 # message =  first + ' [' + last + '] is a coder'
