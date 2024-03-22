0f384301b46ccbe6d7e5dedba21de40e222dcc0f
Mohit kumar paul <mohit1paul@gmail.com>
2024-03-23 00:06:00 +0530

    sclic

--- hello.py
+++ hello.py
@@ -1,1 +1,47 @@
-print('hello world')
+h1 = [2,5,7]
+# h2 = h1[:]      # using slice method where generate another copy 
+# print(h1)
+# print(h2)
+
+# h1[1] = 343
+# print(h1)       
+# print(h2)
+ 
+
+
+
+
+import copy
+h2 = copy.copy(h1)
+print(h2)
+
+h1[1] = 343
+print(h2)
+print(h1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# print('hello world')
