6e75f84b3e244fb5ab8cdceb6f9ccd90bff8bdef
Mohit kumar paul <mohit1paul@gmail.com>
2024-03-23 00:49:13 +0530

    import random =

--- hello.py
+++ hello.py
@@ -19,10 +19,12 @@ h1[1] = 343
 print(h2)
 print(h1)
 
-
-
-
-
+import random
+l1 = ['lemon','tomato' , 'coffe']
+print (random.choice(l1))
+print (random.choice(l1))
+print (random.choice(l1))             #random choose 
+print (random.choice(l1))
 
 
 
