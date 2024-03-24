54b7befae1984d245225a5ab6c01efefb894a780
Mohit kumar paul <mohit1paul@gmail.com>
2024-03-24 21:31:39 +0530

    celsiousToF converting

---
+++ celsioustoF.py
@@ -0,0 +1,8 @@
+#taking input from user
+
+celsious_tem = float (input ("Enter temrature in celsious : "))
+
+#converting into it F
+
+ferhenhit_tem = (celsious_tem * 9/5) +32
+print("Temperature in Fahrenheit : ", ferhenhit_tem)
