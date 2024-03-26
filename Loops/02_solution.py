91930a91a04b2da2d99a4558320976077e7a50be
Mohit kumar paul <mohit1paul@gmail.com>
2024-03-26 13:42:20 +0530

    sum of even numbers

---
+++ Loops/02_solution.py
@@ -0,0 +1,9 @@
+numbers = 10
+
+sum_even_numbers = 0
+for i in range(1, numbers+1) :
+    if i % 2 ==0:                       #sum of even numbers
+        sum_even_numbers +=i
+print("sum_of_even_numbers : ", sum_even_numbers)
+    
+        
