a9b7ba670ec05b58e324c969cced81f52dcb0d12
Mohit kumar paul <mohit1paul@gmail.com>
2024-03-26 23:31:20 +0530

    multiplication table for a given number

---
+++ Loops/03_solution.py
@@ -0,0 +1,7 @@
+number = 7
+
+for i in range(1, 11):       #print the multiplication table for a given number upto 10 
+
+    if i == 5:
+        continue
+    print( number, 'x' , i, '=', number * i )
