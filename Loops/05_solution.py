a6781fd43094aae16f71be7cefbfb488f03e1f92
Mohit kumar paul <mohit1paul@gmail.com>
2024-03-27 23:26:42 +0530

    Enter a number between 1 and 10

---
+++ Loops/05_solution.py
@@ -0,0 +1,8 @@
+while True :
+    number = int(input("Enter a number between 1 and 10 : "))
+
+    if number >=1 and number <=10:
+
+        print("you are entered valid number")
+    else:
+        print ("you are entered Non-valid number")
