164f51c28df1c6f6cd56c31d51964a7acbfc21da
Mohit kumar paul <mohit1paul@gmail.com>
2024-03-28 21:33:08 +0530

    Function with args

---
+++ Fucntions/03_solution.py
@@ -0,0 +1,7 @@
+def multiply (num1, num2):
+    return num1 * num2
+
+print(multiply(6,7))
+print(multiply('a', 5))
+print(multiply(5, 'b'))
+
---
+++ Fucntions/04_Solution.py
@@ -0,0 +1,9 @@
+def sum_all (*args):    # * it takes multiple arguments 
+    print(args)
+    for i in args :
+        print(i *2)
+    return sum (args)
+    
+print(sum_all ( 1,3,4))               
+print(sum_all ( 1,3,4,9,7,8,3))
+print(sum_all ( 1,3,4,4,6,9,6))
