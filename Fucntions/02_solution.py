15553d77a12c5814b05827d44501b0202394e29c
Mohit kumar paul <mohit1paul@gmail.com>
2024-03-28 18:43:44 +0530

    take two numbers from the user and print the sum of two numbers

---
+++ Fucntions/02_solution.py
@@ -0,0 +1,9 @@
+numOne = int (input("Enter an number one : "))
+numTwo = int (input("Enter an number Two  : "))
+
+def add_two_num (numOne,numTwo):
+    return numOne +numTwo
+
+result = add_two_num(numOne,numTwo)  # take two numbers from the user and print the sum of two numbers 
+
+print(result)
