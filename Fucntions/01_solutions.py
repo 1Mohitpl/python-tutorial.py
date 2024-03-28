48a1634dc51b0e315a7c88ea2437dc48e8871ae7
Mohit kumar paul <mohit1paul@gmail.com>
2024-03-28 18:35:02 +0530

    Take an input from user and print the square of a number

---
+++ Fucntions/01_solutions.py
@@ -0,0 +1,7 @@
+number = int(input("Enter an number : "))
+
+def square (number): 
+    return number ** 2
+    
+result = square(number)               #Take an input from user and print the square of a number
+print(result)
