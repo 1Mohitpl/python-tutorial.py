dbba929408b856a955aba68dcda5d411c40cb7ba
Mohit kumar paul <mohit1paul@gmail.com>
2024-04-01 22:43:56 +0530

    add method to the car class displays the full name of the car

--- opps/01_opps.py
+++ opps/01_opps.py
@@ -2,13 +2,18 @@ class Car:
     def __init__(self, Brand, Model):
         self.Brand = Brand
         self.Model = Model
+    
+    def full_name(self):                          #add method to the car class displays the full name of the car
+        return f"{self.Brand} {self.Model}"
 
 
 my_car = Car("Mahindra", "Thar")                 #create a car class with attributes like brand and model
 print (my_car.Brand)
 
 print(my_car.Model)
+print(my_car.full_name())
 
 myNew_car = Car("Tata" , "safari")
 print(myNew_car.Brand)
 print (myNew_car.Model)
+print(myNew_car.full_name())
