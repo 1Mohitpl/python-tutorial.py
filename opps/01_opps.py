3b5d23f3b950731b01a5cad6fa85a5ced1307d19
Mohit kumar paul <mohit1paul@gmail.com>
2024-04-01 21:51:59 +0530

    create a car class with attributes like brand and model

---
+++ opps/01_opps.py
@@ -0,0 +1,10 @@
+class Car:
+    def __init__(self, Brand, Model):
+        self.Brand = Brand
+        self.Model = Model
+
+
+my_car = Car("Mahindra", "Thar")                 #create a car class with attributes like brand and model
+print (my_car.Brand)
+
+print(my_car.Model)
