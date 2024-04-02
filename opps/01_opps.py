f08d8c07f3cf3e8e7edbe004d918bf932d029428
Mohit kumar paul <mohit1paul@gmail.com>
2024-04-02 20:23:18 +0530

    getter setter

--- opps/01_opps.py
+++ opps/01_opps.py
@@ -1,10 +1,16 @@
 class Car:
     def __init__(self, Brand, Model):
-        self.Brand = Brand
+        self.__Brand = Brand
         self.Model = Model
+
+
+
+
+    def  get_brand (self):
+        return self.__Brand + "!"
     
     def full_name(self):                          #add method to the car class displays the full name of the car
-        return f"{self.Brand} {self.Model}"
+        return f"{self.__Brand} {self.Model}"
 
 class ElectricCar(Car):
     def __init___(self, Brand,Model,battery_life):
@@ -12,17 +18,19 @@ class ElectricCar(Car):
         self.battery_life = battery_life
 
 my_tesla = ElectricCar("Tesla" , "modelv")
-print(my_tesla.Brand)
-print(my_tesla.Model)
-print(my_tesla.full_name())
+# print(my_tesla.__Brand)
+# print(my_tesla.Model)
+# print(my_tesla.full_name())
+print(my_tesla.get_brand())
 
 my_car = Car("Mahindra", "Thar")                 #create a car class with attributes like brand and model
-print (my_car.Brand)
+# print (my_car.Brand)
 
-print(my_car.Model)
-print(my_car.full_name())
+# print(my_car.Model)
+# print(my_car.full_name())
+print(my_car.get_brand())
 
 myNew_car = Car("Tata" , "safari")
-print(myNew_car.Brand)
-print (myNew_car.Model)
-print(myNew_car.full_name())
+# print(myNew_car.Brand)
+# print (myNew_car.Model)
+# print(myNew_car.full_name())
