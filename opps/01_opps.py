24ddcd4bedefc3cad8024cb636247063dac41331
Mohit kumar paul <mohit1paul@gmail.com>
2024-04-02 20:53:17 +0530

    polymorphisim

--- opps/01_opps.py
+++ opps/01_opps.py
@@ -12,25 +12,36 @@ class Car:
     def full_name(self):                          #add method to the car class displays the full name of the car
         return f"{self.__Brand} {self.Model}"
 
+    def fuel_type(self):
+        return "petrol or diesel"
+
 class ElectricCar(Car):
     def __init___(self, Brand,Model,battery_life):
         super().__init__(Brand,Model)                 #inheritance 
         self.battery_life = battery_life
 
+    def fuel_type(self):
+        return "Electric charging"
+
 my_tesla = ElectricCar("Tesla" , "modelv")
 # print(my_tesla.__Brand)
-# print(my_tesla.Model)
-# print(my_tesla.full_name())
+print(my_tesla.Model)
+print(my_tesla.full_name())
 print(my_tesla.get_brand())
+print(my_tesla.fuel_type())
 
 my_car = Car("Mahindra", "Thar")                 #create a car class with attributes like brand and model
 # print (my_car.Brand)
 
-# print(my_car.Model)
-# print(my_car.full_name())
+print(my_car.Model)
+print(my_car.full_name())
 print(my_car.get_brand())
 
 myNew_car = Car("Tata" , "safari")
 # print(myNew_car.Brand)
-# print (myNew_car.Model)
-# print(myNew_car.full_name())
+print (myNew_car.Model)
+print(myNew_car.full_name())
+print(myNew_car.get_brand())
+
+advanture = Car("Tata", "safari")
+print(advanture.fuel_type())
