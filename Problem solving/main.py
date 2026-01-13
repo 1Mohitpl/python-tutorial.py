7fc4d2210d7cc90be72d4847ca33415a352168d3
Mohit kumar paul <mohit1paul@gmail.com>
2026-01-13 23:20:48 +0530

    practice python

---
+++ Problem solving/main.py
@@ -0,0 +1,11 @@
+age = int(input("Enter your age :"))
+
+
+if age < 13:
+    print("you are not an chaild")
+elif age < 20:
+    print("You are teenager")
+elif age  <30:
+    print("young adult")
+else:
+    print("You are senior")
