bdf2fac47b25b74a899379d440f397ce3c6a86ac
Mohit kumar paul <mohit1paul@gmail.com>
2026-01-28 21:42:24 +0530

    sets

---
+++ sets/learn_sets.py
@@ -0,0 +1,9 @@
+set = {10, 56, 89, 90, 'mohit', True}
+print(set)
+# sets are unordered, 
+#in sets duplicacy is not allowd
+# in sets, indexing access is not allowed
+# in sets, we cannot modifiy single item, instead we can add one iteam at a time by the "add" method
+# in sets, we cannnot add list in a sets beacause list are immutable.
+set.add(23)
+print(set)
