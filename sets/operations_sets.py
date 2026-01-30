c492b3abafb91df3f493686ac070d94d05e484b6
Mohit kumar paul <mohit1paul@gmail.com>
2026-01-30 22:09:11 +0530

    sets

---
+++ sets/operations_sets.py
@@ -0,0 +1,5 @@
+set1 = {"Mohit", "Rahul", "Boni"}
+set2 = {"jhon", "Dev", "Mohit"}
+
+print(set1.union(set2))  # here invoked union methods on set1 and passing set2 as argument. If you have one more set, then pass another set as arguments
+print(set1.intersection(set2)) # Get the value which is present in both the Set
