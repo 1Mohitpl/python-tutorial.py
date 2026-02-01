f61ce5ecbe32b846e22a5105d51a71dc695226ab
Mohit kumar paul <mohit1paul@gmail.com>
2026-02-01 20:21:35 +0530

    for loops

---
+++ Loops/07_solution.py
@@ -0,0 +1,10 @@
+# using for-else to check if statement
+numbers  = [1, 2, 3, 4, 5]
+
+for i in numbers:
+    print(i)
+    if(i ==4):
+        break
+else :
+    print("all are Nmbers here")
+print("I found the key")
